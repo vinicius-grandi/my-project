b1457d57b8e8cb1a69cff03888ca2f2945c0c216
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-02-01 18:43:25 -0300

    um pouco de style-components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import styled from 'styled-components';
+import { useState } from 'react'
 
+const Title = styled.h3`
+  font-size: ${({ fontSize }) => fontSize}px;
+  color: ${({ theme }) => theme.colors.primary};
+
+  a {
+    color: yellow;
+    &:hover {
+      color: black;
+    }
+  }
+`
 
 function App() {
+
+  const [fontSize, setFontSize] = useState(30)
   return (
     <>
-      
+      <h1>App</h1>
+      <Title fontSize={fontSize}>Que maravilha de componente estilizado
+        <p><a href="http://localhost">Jaime</a></p>
+      </Title>
+      <button onClick={() => (setFontSize(fontSize + 1))}>Aumentar Texto </button>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,12 +5,21 @@ import App from './App';
 import store from './app/store'
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
+
+const theme = {
+  colors: {
+    primary: '#0070f3'
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <Provider store={store}>
       <App />
     </Provider>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
